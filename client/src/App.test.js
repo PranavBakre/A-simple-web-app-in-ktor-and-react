4e54891fb394f90719cbf444cded128567aee140
Pranav Bakre <psbakre@yahoo.com>
2021-02-02 16:00:23 +0530

    deleted test

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("Home Page", () => {
-  const Render = render(<App />);
-  const linkElement = Render.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
