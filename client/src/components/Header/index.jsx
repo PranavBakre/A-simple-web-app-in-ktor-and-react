0bbef8aad180da85d4525fbb71178aefa8b7555d
Pranav Bakre <pranavbakre@yahoo.in>
2022-04-30 20:54:32 +0530

    feat(client): Independent Header Component

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import css from "./index.module.css";
+
+function Header() {
+  const { user } = useSelector((state) => state.auth);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className={css.header}>
+      <div className={css.title}>A simple React App</div>
+      <div className={css.popper} onClick={() => setMenuOpen(!isMenuOpen)}>
+        <img
+          className={css.profilePhoto}
+          src={user.profilePicture}
+          alt=""
+          referrerPolicy="no-referrer"
+        />
+        <div className={css.popperBody} style={{visibility: isMenuOpen?"visible":"hidden"}}>
+          <div>
+          <div className={css.popperItem}>
+            Profile
+          </div>
+          
+          <div className={css.popperItem}>
+            Logout
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
